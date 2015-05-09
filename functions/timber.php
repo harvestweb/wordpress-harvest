a096d90e66a54dcdf43949e92191421a8baf78ee
Matt Rose <rose.matt@gmail.com>
2015-05-09 00:37:01 -0400

    Replace header menu with short menu

--- functions/timber.php
+++ functions/timber.php
@@ -43,6 +43,7 @@ class MySite extends TimberSite {
     $context['stuff'] = 'I am a value set in your functions.php file';
     $context['notes'] = 'These values are available everytime you call Timber::get_context();';
     $context['menu'] = new TimberMenu();
+    $context['short_menu'] = new TimberMenu('Short');
     $context['site'] = $this;
     return $context;
   }
